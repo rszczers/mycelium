8e2a85034cf581f9473f1c40ff8cfad7c13c639d
Rafał Szczerski <rafal.szczerski@gmail.com>
2017-03-10 17:39:03 +0100

    cells

--- src/VectorField.java
+++ src/VectorField.java
@@ -1,19 +1,25 @@
-import processing.core.PApplet;
+import processing.core.PVector;
+
 /**
  * Created by rszczers on 10.03.17.
  */
 public class VectorField {
-    private float[][] block;
+    private PVector[][] block;
 
     public VectorField(int n) {
-       block = new float[n][n];
+        block = new PVector[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                block[i][j] = PVector.random2D().mult(20.0f);
+            }
+        }
     }
 
-    public void setBlock(int[] coord, float v) {
+    public void setBlock(int[] coord, PVector v) {
         block[coord[0]][coord[1]] = v;
     }
 
-    public float[][] getBlock() {
+    public PVector[][] getBlock() {
         return block;
     }
 }
--- src/mycelium.java
+++ src/mycelium.java
@@ -1,9 +1,15 @@
 import processing.core.PApplet;
+import processing.core.PVector;
 
 public class mycelium extends PApplet {
     private static int WIDTH = 800;
     private static int HEIGHT = 600;
     private static int GRID = 8;
+    private boolean drawCells = true;
+    private boolean drawGrids = true;
+    private boolean drawVectorFields = true;
+
+    private static float[][][] cellColor = new float[GRID][GRID][3];
 
     LSystem lsystem;
     VectorField vf = new VectorField(GRID);
@@ -16,14 +22,28 @@ public class mycelium extends PApplet {
     }
 
     public void setup() {
-
+        frameRate(30);
+        for (int i = 0; i < GRID; i++) {
+            for (int j = 0; j < GRID; j++) {
+                for (int k = 0; k < 3; k++) {
+                    cellColor[i][j][k] = 100 + vf.getBlock()[i][j].dist(new PVector(0, 0, 0));
+                }
+            }
+        }
     }
 
     public void draw() {
         background(180);
-        drawGrid();
-        drawVectorField(vf);
-        vf.setBlock(new int[] {mouseX/(WIDTH/GRID), mouseY/(HEIGHT/GRID)}, 1);
+        //vf.setBlock(new int[] {mouseX/(WIDTH/GRID), mouseY/(HEIGHT/GRID)}, PVector.random2D());
+
+        if (drawCells)
+            drawCells();
+        if (drawGrids)
+            drawGrid();
+        if (drawVectorFields)
+            drawVectorField(vf);
+
+        fill(0, 102, 153);
         text(mouseX/(WIDTH/GRID) + "; " + mouseY/(HEIGHT/GRID) + "\n" , width / 2, 60);
     }
 
@@ -42,14 +62,25 @@ public class mycelium extends PApplet {
         for (int i = 0; i < vf.getBlock().length; i++) {
             for (int j = 0; j < vf.getBlock().length; j++) {
                 int[] c = vf2c(i, j);
-                int x1 = c[0]+(WIDTH/(2*GRID));
-                int y1 = c[1]+(HEIGHT/(2*GRID));
-                int x2 = x1+5;
-                int y2 = y1+5;
+                int x1 = c[0] + (WIDTH / (2 * GRID));
+                int y1 = c[1] + (HEIGHT / (2 * GRID));
+                int x2 = x1 + (int) vf.getBlock()[i][j].x;
+                int y2 = y1 + (int) vf.getBlock()[i][j].y;
                 line(x1, y1, x2, y2);
             }
         }
+        noStroke();
+    }
 
+    private void drawCells() {
+        int dw = WIDTH/GRID;
+        int dh = HEIGHT/GRID;
+        for (int x = 0; x < WIDTH; x += dw) {
+            for (int y = 0; y < HEIGHT; y += dh) {
+                fill(cellColor[x/dw][y/dh][0], cellColor[x/dw][y/dh][1], cellColor[x/dw][y/dh][2]);
+                rect(x, y, WIDTH/GRID, HEIGHT/GRID);
+            }
+        }
     }
 
     private int[] c2vf(int x, int y) {
